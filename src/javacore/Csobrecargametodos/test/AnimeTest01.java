b8b2c03246079ada81b025627c84490218c01f6e
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-23 11:58:48 -0300

    Sobrecarga de métodos

---
+++ src/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package javacore.Csobrecargametodos.test;
+
+import javacore.Csobrecargametodos.model.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+        anime.iniciar("Naruto", "TV", 12, "Ação");
+        anime.imprime();
+    }
+}
