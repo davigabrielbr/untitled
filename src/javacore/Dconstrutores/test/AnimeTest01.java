a899ecbf4363d20e13ec47531761801de3b4faae
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-23 13:14:39 -0300

    Construtores

--- src/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,9 +5,7 @@ import javacore.Dconstrutores.model.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime("Naruto", "TV", 12, "Ação", "Production IG");
-        Anime anime2 = new Anime("Teste", "TV", 15, "Terror", "Estudio");
-        
+
         anime.imprime();
-        anime2.imprime();
     }
 }
