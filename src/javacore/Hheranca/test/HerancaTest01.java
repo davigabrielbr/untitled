427a96e4632b82124b09d77100a88ed7b513bd02
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-01-17 16:06:26 -0300

    Herança

--- src/javacore/Hheranca/test/HerancaTest01.java
+++ src/javacore/Hheranca/test/HerancaTest01.java
@@ -7,19 +7,17 @@ import javacore.Hheranca.model.Pessoa;
 public class HerancaTest01 {
     public static void main(String[] args) {
         Endereco endereco = new Endereco();
-        Pessoa pessoa = new Pessoa();
-        Funcionario funcionario = new Funcionario();
+        Pessoa pessoa = new Pessoa("Davi Gabriel");
+        Funcionario funcionario = new Funcionario("Lucas Matheus");
 
         endereco.setRua("Rua amambai");
         endereco.setCep("012345-678");
 
-        pessoa.setNome("Davi Gabriel");
         pessoa.setCpf("1234567890");
         pessoa.setEndereco(endereco);
 
         pessoa.imprime();
 
-        funcionario.setNome("Lucas Matheus");
         funcionario.setCpf("12345");
         funcionario.setEndereco(endereco);
         funcionario.setSalario(20000);
