37be55598180bfe97907e4853d0652ce0c259024
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-01-28 11:03:35 -0300

    Enumeração

--- src/javacore/Kenum/test/ClienteTest01.java
+++ src/javacore/Kenum/test/ClienteTest01.java
@@ -5,16 +5,10 @@ import javacore.Kenum.model.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Davi", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Davi", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Davi", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Davi", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente5 = new Cliente("Davi", TipoCliente.PESSOA_JURIDICA2);
+        Cliente cliente1 = new Cliente("Davi", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Lucas", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
-        System.out.println(cliente5);
     }
 }
